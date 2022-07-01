6789c963df4af967ed6712f2562e725dc2618b35
Yemi George <yg@yg.com>
2022-06-30 20:46:48 -0600

    Updated - reverse list order

--- reverse_list_order.js
+++ reverse_list_order.js
@@ -1,10 +1,8 @@
 // This function takes an array/list and returns it in reversed order
-<<<<<<< HEAD
 //Where x is the input array and y the result
 //8 kyu
-=======
 //Where x is the input array and y the result. 8 kyu
->>>>>>> 8017d7fc50402e573e5f29df3f705c02a3819ad3
+
 function reverse(x){
     let y = [];
     for (let i = x.length - 1; i > -1; i--){
